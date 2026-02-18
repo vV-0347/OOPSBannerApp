a757a8ec569b29931e5170ac2a352d17008b5944
vV-0347 <vidyuthsumanth1@gmail.com>
2026-02-18 15:28:00 +0530

    src added for assci art

--- src/OOPSBanner.java
+++ src/OOPSBanner.java
@@ -1,5 +1,21 @@
 public class OOPSBanner {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // UC2: Manual string concatenation and sequential print statements
+        // 7-line grid (7 rows) to display stylized "OOPS"
+        
+        // Line 1
+        System.out.println(" ***     ***    *****    ***    ");
+        // Line 2
+        System.out.println("*   *   *   *   *       *   *   ");
+        // Line 3
+        System.out.println("*   *   *   *   *       *   *   ");
+        // Line 4
+        System.out.println("*   *   *   *   ****    *   *   ");
+        // Line 5
+        System.out.println("*   *   *   *   *       *   *   ");
+        // Line 6
+        System.out.println("*   *   *   *   *       *   *   ");
+        // Line 7
+        System.out.println(" ***     ***    *****    ***    ");
     }
 }
