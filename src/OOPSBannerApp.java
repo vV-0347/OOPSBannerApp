db8fa5961589adcfea77337c1ee2bb49d139e3ae
vV-0347 <vidyuthsumanth1@gmail.com>
2026-03-01 01:01:32 +0530

    UC6 - Refactor banner logic into static helper methods

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -2,60 +2,62 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
 
-            String.join("   ",
-                "  *****  ",
-                "  *****  ",
-                " ******  ",
-                " ******   "
-            ),
+        String[] banner = new String[7];
 
-            String.join("   ",
-                " **   ** ",
-                " **   ** ",
-                " **   ** ",
-                " **      "
-            ),
-
-            String.join("   ",
-                " **   ** ",
-                " **   ** ",
-                " **   ** ",
-                " **      "
-            ),
-
-            String.join("   ",
-                " **   ** ",
-                " **   ** ",
-                " ******  ",
-                "  *****  "
-            ),
-
-            String.join("   ",
-                " **   ** ",
-                " **   ** ",
-                " **      ",
-                "     **  "
-            ),
-
-            String.join("   ",
-                " **   ** ",
-                " **   ** ",
-                " **      ",
-                "     **  "
-            ),
-
-            String.join("   ",
-                "  *****  ",
-                "  *****   ",
-                "**      ",
-                " ******  "
-            )
-        };
+        for (int i = 0; i < 7; i++) {
+            banner[i] = String.join("   ",
+                    o[i],
+                    o[i],
+                    p[i],
+                    s[i]
+            );
+        }
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    // Helper method for O
+    public static String[] getOPattern() {
+        return new String[] {
+                "  *****  ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                " **   ** ",
+                "  *****  "
+        };
+    }
+
+    // Helper method for P
+    public static String[] getPPattern() {
+        return new String[] {
+                " ******  ",
+                " **   ** ",
+                " **   ** ",
+                " ******  ",
+                " **      ",
+                " **      ",
+                " **      "
+        };
+    }
+
+    // Helper method for S
+    public static String[] getSPattern() {
+        return new String[] {
+                "  *****  ",
+                " **      ",
+                " **      ",
+                "  *****  ",
+                "      ** ",
+                "      ** ",
+                "  *****  "
+        };
     }
 }
