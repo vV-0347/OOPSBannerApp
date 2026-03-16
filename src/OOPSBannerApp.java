c61cfec0bfaa39a0fe9e434e220032139cd82fb1
vV-0347 <vidyuthsumanth1@gmail.com>
2026-03-16 11:26:02 +0530

    UC8 - Resolve merge conflict and implement HashMap banner rendering

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,18 +1,76 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class OOPSBannerApp {
+
+    /**
+     * Utility method to create and return the character pattern map
+     */
+    public static Map<Character, String[]> createCharacterPatterns() {
+
+        Map<Character, String[]> map = new HashMap<>();
+
+        map.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        map.put('P', new String[]{
+                "*****  ",
+                "*    * ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      ",
+                "*      "
+        });
+
+        map.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        });
+
+        return map;
+    }
+
+    /**
+     * Method to display banner message
+     */
+    public static void renderBanner(String message, Map<Character, String[]> map) {
+
+        for (int row = 0; row < 7; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : message.toCharArray()) {
+
+                String[] pattern = map.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
 
     public static void main(String[] args) {
 
-        System.out.println(createLine("*","*","*","*","*","   ","*","*","*","*","*","   ","*","*","*","*","*","   ","*","*","*","*","*"));
-        System.out.println(createLine("*","       ","*","   ","*","       ","*","   ","*","       ","*","   ","*","       ","*"));
-        System.out.println(createLine("*","       ","*","   ","*","       ","*","   ","*","       ","*","   ","*","       ","*"));
-        System.out.println(createLine("*","       ","*","   ","*","*","*","*","*","   ","*","*","*","*","*","   ","*","*","*","*","*"));
-        System.out.println(createLine("*","       ","*","   ","*","       ","   ","*","       ","   ","*","       ","*"));
-        System.out.println(createLine("*","       ","*","   ","*","       ","   ","*","       ","   ","*","       ","*"));
-        System.out.println(createLine("*","*","*","*","*","   ","*","*","*","*","*","   ","*","       ","   ","*","*","*","*","*"));
-    }
+        String word = "OOPS";
 
-    // Reusable method using String.join()
-    public static String createLine(String... parts) {
-        return String.join("", parts);
+        Map<Character, String[]> characterPatterns = createCharacterPatterns();
+
+        renderBanner(word, characterPatterns);
     }
 }
